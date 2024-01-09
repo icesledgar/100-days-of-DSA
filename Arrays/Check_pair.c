acbd64a5aee7e420fa34e3adf12685732a3f1e10
icesledgar <aditya19032003@gmail.com>
2024-01-09 13:40:28 +0530

    gitignore update

---
+++ Arrays/Check_pair.c
@@ -0,0 +1,36 @@
+/*
+* This C program tells if there exists a pair in array
+* whose sum results in x.
+*/
+
+#include <stdio.h>
+
+// Function to find and print pair
+int checkPair(int A[], int size, int x)
+{
+	for (int i = 0; i < (size - 1); i++) {
+		for (int j = (i + 1); j < size; j++) {
+			if (A[i] + A[j] == x) {
+				return 1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int main(void)
+{
+	int A[] = { 0, -1, 2, -3, 1 };
+	int x = -2;
+	int size = sizeof(A) / sizeof(A[0]);
+
+	if (checkPair(A, size, x)) {
+		printf("Yes\n");
+	}
+	else {
+		printf("No\n");
+	}
+
+	return 0;
+}
